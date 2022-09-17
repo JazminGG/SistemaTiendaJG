d0a8a3b603693ce55600634b69260730bfa51fb4
Jazmin Gaytan <jazmincaroggomez@gmail.com>
2022-09-17 02:00:40 -0500

    Se modifico la clase Funciones

--- Manejador.TiendaGit/Funciones.cs
+++ Manejador.TiendaGit/Funciones.cs
@@ -6,6 +6,15 @@ namespace Manejador.TiendaGit
 {
     public class Funciones
     {
-        
+        private TiendaAccesoDatos _tiendaAccesoDatos = new TiendaAccesoDatos();
+        public List<Productos> GetProductos(string dato)
+        {
+            var listProductos = _tiendaAccesoDatos.GetProductos(dato);
+            return listProductos;
+        }
+        public void GuardarProducto(Productos producto)
+        {
+            _tiendaAccesoDatos.GuardarProducto(producto);
+        }
     }
 }
