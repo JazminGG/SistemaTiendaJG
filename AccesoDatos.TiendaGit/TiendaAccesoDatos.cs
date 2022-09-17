2042a4f35b64040ecd4c5b991362be3e65a5fec4
Jazmin Gaytan <jazmincaroggomez@gmail.com>
2022-09-17 01:51:33 -0500

    Se modifico la clase TiendaAccesoDatos

--- AccesoDatos.TiendaGit/TiendaAccesoDatos.cs
+++ AccesoDatos.TiendaGit/TiendaAccesoDatos.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Entidades.TiendaGit;
 
 namespace AccesoDatos.TiendaGit
@@ -15,7 +12,35 @@ namespace AccesoDatos.TiendaGit
         {
             con = new Conexion("localhost", "root", "", "Sistema", 3306);
         }
+        public void GuardarProducto(Productos producto)
+        {
+            string consulta = string.Format("Insert into productos values({0},'{1}','{2}',{3})",
+                producto.IdProducto, producto.Nombre, producto.Descripcion, producto.Precio);
+            con.EjecutarConsulta(consulta);
+        }
+        public List<Productos> GetProductos(string dato)
+        {
+            var ListProductos = new List<Productos>();
+            var ds = new DataSet();
 
-        
+            string consulta = string.Format("select * from productos where nombre like '%{0}%'", dato);
+            ds = con.ObtenerDatos(consulta, "productos");
+
+            var dt = new DataTable();
+            dt = ds.Tables[0];
+
+            foreach (DataRow row in dt.Rows)
+            {
+                var Productos = new Productos
+                {
+                    IdProducto = Convert.ToInt32(row["IdProducto"]),
+                    Nombre = row["Nombre"].ToString(),
+                    Descripcion = row["Descripcion"].ToString(),
+                    Precio = Convert.ToInt32(row["Precio"])
+                };
+                ListProductos.Add(Productos);
+            }
+            return ListProductos;
+        }
     }
 }
